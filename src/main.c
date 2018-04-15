a10bdc483fb2a03ba9764dc8edc60ef9ac4d2dee
Ishaan Verma <ishaanverma97@gmail.com>
2018-04-15 22:02:28 +0530

    Added tokenize function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 #define MAX_HIS_SIZE 100
+#define MAX_TOK_SIZE 25
 
 int status = 1;
 char *history[MAX_HIS_SIZE];
@@ -25,6 +27,7 @@ void add_history(char *line)    {
 void get_history()  {
     int i;
     printf("----------HISTORY-----------\n");
+
     for(i=front; i < MAX_HIS_SIZE; i++) {
         if (*(history + i)) {
             printf("%s\n", *(history + i));
@@ -33,9 +36,11 @@ void get_history()  {
             break;
         }
     }
+
     for(i=0; i < front; i++)    {
         printf("%s\n", *(history + i));
     }
+
     printf("----------------------------\n");
 }
 
@@ -70,15 +75,47 @@ char *read_line()   {
 
 }
 
+char *tokenize(char *line, char delimiter)  {
+    int length = strlen(line);
+    int i = 0, tokens = 0;
+    char *new_line = NULL;
+    char *args =  NULL;
+
+    args = (char *) malloc(sizeof(char) * MAX_TOK_SIZE);
+    new_line = (char *) realloc(line, (sizeof(char) * length));
+
+    if (!(*new_line))
+        return ' ';
+
+    args[tokens] = *new_line;
+    tokens++;
+    for (i=0; i < length; i++)  {
+        if (new_line[i] == delimiter)   {
+            new_line[i] = '\0';
+
+            if (new_line[i+1])    {
+                args[tokens] = *new_line + (i * sizeof(char)) + 1;
+                tokens++;
+            }
+        }
+    }
+
+    for(i=0; i < tokens; i++)   {
+        printf("%s\n", *(args + i));
+    }
+
+
+}
+
 int main(int argc, char *argv[])    {
     char *line, *token;
 
     do  {
 
         line = read_line();
-        //token = tokenize(line);
+        tokenize(line, ' ');
         //printf("%s\n", line);
-        get_history();
+        //get_history();
     } while(status);
 
 
