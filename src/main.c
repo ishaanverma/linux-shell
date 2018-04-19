a4920ee7d1a804c0b55095d97145f736cc2baeef
Ishaan Verma <ishaanverma97@gmail.com>
2018-04-20 03:14:20 +0530

    Fixed tokenize function and added execprogram function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
 #define MAX_HIS_SIZE 100
@@ -75,45 +77,64 @@ char *read_line()   {
 
 }
 
-char *tokenize(char *line, char delimiter)  {
+char **tokenize(char *line, char delimiter)  {
+
+    static char *args[MAX_TOK_SIZE];
     int length = strlen(line);
     int i = 0, tokens = 0;
-    char *new_line = NULL;
-    char *args =  NULL;
+    char *new_line = (char *) NULL;
 
-    args = (char *) malloc(sizeof(char) * MAX_TOK_SIZE);
-    new_line = (char *) realloc(line, (sizeof(char) * length));
+
+    new_line = realloc(line, (sizeof(char) * length));
 
     if (!(*new_line))
-        return ' ';
+        return NULL;
 
-    args[tokens] = *new_line;
+    args[tokens] = new_line;
     tokens++;
     for (i=0; i < length; i++)  {
         if (new_line[i] == delimiter)   {
             new_line[i] = '\0';
 
-            if (new_line[i+1])    {
-                args[tokens] = *new_line + (i * sizeof(char)) + 1;
+            if (new_line[i+1] != delimiter)    {
+                args[tokens] = new_line + (i * sizeof(char)) + 1;
                 tokens++;
             }
         }
     }
 
-    for(i=0; i < tokens; i++)   {
-        printf("%s\n", *(args + i));
+    return args;
+}
+
+int execprogram(char **argu)    {
+    int pid;
+    pid = fork();
+
+    //child
+    if (pid == 0)   {
+        int r;
+        r = execvp(argu[0], argu);
+
+        if (r == -1)    {
+            printf("Unkown Command\n");
+        }
+        exit(0);
     }
-
-
+    //parent
+    else    {
+        wait(&pid);
+        return 1;
+    }
 }
 
 int main(int argc, char *argv[])    {
-    char *line, *token;
+    char *line, *token, **argu;
 
     do  {
 
         line = read_line();
-        tokenize(line, ' ');
+        argu = tokenize(line, ' ');
+        execprogram(argu);
         //printf("%s\n", line);
         //get_history();
     } while(status);
